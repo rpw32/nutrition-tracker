ab3deb1a4bdae63bb54a74f9c13cc31ad6a25783
Riley Wagner <riley.wagner@siu.edu>
2021-10-15 15:45:28 -0500

    fixed the api routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 import { RouteReuseStrategy } from '@angular/router';
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppRoutingModule } from './app-routing.module';
@@ -8,7 +9,7 @@ import { AppComponent } from './app.component';
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
--- src/app/shared-components/product/product.component.ts
+++ src/app/shared-components/product/product.component.ts
@@ -1,6 +1,10 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { ToastController } from '@ionic/angular';
 import { GroceriesService } from 'src/app/services/groceries.service';
+import { ApiService } from 'src/app/api.service';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-product',
@@ -10,9 +14,45 @@ import { GroceriesService } from 'src/app/services/groceries.service';
 export class ProductComponent implements OnInit {
 
   @Input() product: any;
-  constructor(private toastCtrl: ToastController, public groceries: GroceriesService) { }
+  constructor(private toastCtrl: ToastController, public groceries: GroceriesService, public api: ApiService) { }
 
   ngOnInit() {}
+
+
+  addRecipe(): Observable<any> {
+    const params = {name: 'Chicken Tikka Masala', price: '12.23'};
+
+    const paramString = JSON.stringify(params);
+    const paramObj = JSON.parse(paramString);
+
+    console.log('Entered the addRecipe function');
+
+
+    // set headers
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type':  'application/json'
+      })
+    };
+
+    console.log('returning api');
+    // submit form to backend to get name by email
+    return this.api.addRecipe(paramObj, httpOptions).pipe(
+      map(
+        data => {
+          if  ((data !== -1) && (data != null)){
+            console.log('Success');// successfully found a name in the db for this email
+            return data;
+          }
+          else {
+            // couldn't find a name
+            console.log('Failure');
+            return false;
+          }
+        },
+      )
+    );
+  }
 
   async addIngredient(ingredient) {
     const toast = await this.toastCtrl.create({
@@ -23,5 +63,7 @@ export class ProductComponent implements OnInit {
     toast.present();
 
     this.groceries.addToList(ingredient.name);
+
+    this.addRecipe().subscribe();
   }
 }
